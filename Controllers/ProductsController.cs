4a06e9efd1da29d35f0fc6be981fa0636906b363
Choopa <djordje.jokic@gmail.com>
2025-02-05 10:19:58 +0100

    Implementig Pagination, searching, ordering, filtering on the server side.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,15 +2,30 @@
 using RestoreApiV2.Entities;
 using RestoreApiV2.Data;
 using Microsoft.EntityFrameworkCore;
+using RestoreApiV2.RequestHelpers;
+using RestoreApiV2.Extensions;
 
 namespace RestoreApiV2.Controllers
 {
     public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
-            return await context.Products.ToListAsync();
+            var query = context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query,
+                productParams.PageNumber, productParams.PageSize);
+
+            //return Ok(new {Items = products, products.Metadata}); if we are returning metadata through response body
+            
+            Response.AddPaginationHeader(products.Metadata);
+
+            return products;
         }
 
         [HttpGet("{id}")] // api/products/2
@@ -21,6 +36,15 @@ namespace RestoreApiV2.Controllers
             if (product == null) return NotFound();
 
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await context.Products.Select(x => x.Brand).Distinct().ToListAsync();
+            var types = await context.Products.Select(x => x.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
     }
 }
---
+++ Extensions/HttpExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Net.Http.Headers;
+using RestoreApiV2.RequestHelpers;
+using System.Text.Json;
+
+namespace RestoreApiV2.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, PaginationMetadata metadata)
+        {
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            response.Headers.Append("Pagination", JsonSerializer.Serialize(metadata, options));
+            response.Headers.Append(HeaderNames.AccessControlExposeHeaders, "Pagination");
+        }
+    }
+}
